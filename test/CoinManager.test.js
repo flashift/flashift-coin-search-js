c004a19b9cfa0863029034264db426e181d93414
mossafaei <mostafa.safaeipour1@gmail.com>
2025-05-08 12:26:41 +0330

    Fix: remove dependencies

--- test/CoinManager.test.js
+++ test/CoinManager.test.js
@@ -1,3 +1,8 @@
+const trieData = require('../assets/trieData.json');
+
+const coinList = require('../assets/coinList.json');
+const networkList = require('../assets/networkList.json');
+
 const CoinManager = require('../lib/CoinManager');
 
 describe('CoinManager', () => {
@@ -8,29 +13,35 @@ describe('CoinManager', () => {
     });
 
     test('should load a coin file', () => {
-        coinManager.loadCoinFile('./assets/trieData.json')
+        coinManager.loadCoinTrie(trieData);
         expect(true).toBe(true);
     });
 
     test('should generate a coin file', () => {
-        coinManager.generateCoinFile('./assets/coinList.json', './assets/networkList.json', './assets/trieData.json')
+        coinManager.generateCoinTrie(coinList, networkList)
         expect(true).toBe(true);
     });
 
     test('should suggest some coins', () => {
-        coinManager.loadCoinFile('./assets/trieData.json')
+        coinManager.loadCoinTrie(trieData);
+        coinManager.suggest('tether');
+        expect(true).toBe(true);
+    });
+
+    test('should suggest some coins from Object', () => {
+        coinManager.loadCoinTrie(trieData);
         coinManager.suggest('tether');
         expect(true).toBe(true);
     });
 
     test('should suggest some coins with network', () => {
-        coinManager.loadCoinFile('./assets/trieData.json')
+        coinManager.loadCoinTrie(trieData);
         coinManager.suggest('tether', 'bsc');
         expect(true).toBe(true);
     });
 
     test('should suggest some coins with erc20', () => {
-        coinManager.loadCoinFile('./assets/trieData.json')
+        coinManager.loadCoinTrie(trieData);
         coinManager.suggest('tethererc20');
         expect(true).toBe(true);
     });
